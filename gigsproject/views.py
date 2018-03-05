236b0ed2fe11c0f97bf8ccce2bc160cc94c69620
Martina Lofqvist <2265215l@student.gla.ac.uk>
2018-03-05 13:24:40 +0000

    First Commit

---
+++ gigsproject/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Gigs index.")
+
+def about(request):
+    return HttpResponse("This is the about page.")
