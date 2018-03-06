328deb6d73caf5350be83f4189db3bb56e4af0f0
daniela997 <2262058i@student.gla.ac.uk>
2018-03-06 18:13:26 +0000

    chapter error 4 fixed @Julia

--- GlasgowGigs/urls.py
+++ GlasgowGigs/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^gigsproject/', include('gigsproject.urls')),
+    url(r'^glasgowgigs/', include('gigsproject.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about, name='about'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
